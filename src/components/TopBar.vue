4e04348815dda784b0b2f2d238610c6e4db387b2
songwx <songwuxin@live.cn>
2018-07-31 23:58:24 +0800

    edit

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,6 +2,20 @@
 	<div id="topbar">
 		<div class="wrapper">
 			<span class="logo">WXin</span>
+			<div class="actions">
+				<button class="primary">
+					登录
+				</button>
+				<button>
+					登出
+				</button>
+				<button class="primary">
+					保存
+				</button>
+				<button>
+					预览	
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -12,20 +26,40 @@
 	}
 </script>
 
-<style>
+<style scoped lang="scss">
 	#topbar{
 		color: green;
 		background:#ffffff;
 		box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-	}
-	#topbar>.wrapper{
-		height:64px;
-		display: flex;
-		align-items: center;
-		padding: 0 16px;
-	}
-	#topbar .logo{
-		font-size: 24px;
-		color: #000000;
+		>.wrapper{
+			height: 64px;
+			padding: 0 16px;
+		}
+		>.wrapper{
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+		}
+		.logo{
+			font-size: 24px;
+			color: #000000;
+		}
+		button{
+			width: 72px;
+			height: 32px;
+			border: none;
+			cursor: pointer;
+			font-size: 18px;
+			background: #ddd;
+			color: #222;
+			&:hover{
+				box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
+			}
+			&.primary{
+				background:#02af5f;
+				color: white;
+			}
+		}
+
 	}
 </style>
